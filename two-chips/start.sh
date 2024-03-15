c6fa57e45b9734e0e1f0e5e81aed91a1cddfc6a5
Chengcheng-S <87012234+Chengcheng-S@users.noreply.github.com>
2024-03-15 14:15:17 +0800

    test code

--- two-chips/start.sh
+++ two-chips/start.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 set -e
 
+if ! command -v rustc &> /dev/null
+then
+    echo "Rust is not installed, installing Rust..."
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    source ~/.bashrc
+else
+    echo "Rust is already installed"
+fi
+
+
 cargo check && cargo clippy
 
+sleep 5 
+
 cargo run 
+
+sleep 5
+
+echo "uninstall rust env"
+rustup self uninstall
+source ~/.bashrc
+echo "plase restart your terminal && run rustc -V or cargo -V check the rust uninstall"
